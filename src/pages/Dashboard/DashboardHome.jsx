06eb2375e285af7e1b71ee8d32cdab3f6a30e5ad
Thejellyfish1024 <alamin102410@gmail.com>
2023-12-25 14:43:25 +0600

    fixed task counts

--- src/pages/Dashboard/DashboardHome.jsx
+++ src/pages/Dashboard/DashboardHome.jsx
@@ -1,7 +1,14 @@
 import TaskTables from "../../components/TaskTables";
+import useTasks from "../../hooks/useTasks";
 
 
 const DashboardHome = () => {
+
+    const { data } = useTasks();
+    console.log(data);
+    const todoTasks = data?.filter(task => task?.status === 'todo')
+    const ongoingTasks = data?.filter(task => task?.status === 'ongoing')
+    const completedTasks = data?.filter(task => task?.status === 'completed')
 
     return (
         <div className="mt-8 w-4/5 mx-auto">
@@ -9,7 +16,7 @@ const DashboardHome = () => {
                 <div className="flex justify-center">
                     <div className="w-48 h-28 space-y-2 bg-gradient-to-r from-black to-[#FCDBFF]
                 justify-center items-center rounded-lg flex flex-col">
-                        <p className="text-lg font-bold">20</p>
+                        <p className="text-lg font-bold">{todoTasks?.length+ongoingTasks?.length+completedTasks?.length}</p>
                         <h4 className="text-lg font-semibold">Total Tasks</h4>
                     </div>
                 </div>
@@ -17,7 +24,7 @@ const DashboardHome = () => {
                 <div className="flex justify-center">
                     <div className="w-48 h-28 space-y-2 bg-gradient-to-r from-[#BB34F5] to-[#FCDBFF]
                 justify-center items-center rounded-lg flex flex-col">
-                        <p className="text-lg font-bold">20</p>
+                        <p className="text-lg font-bold">{todoTasks?.length}</p>
                         <h4 className="text-lg font-semibold">To-do Tasks</h4>
                     </div>
                 </div>
@@ -25,7 +32,7 @@ const DashboardHome = () => {
                 <div className="flex justify-center">
                     <div className="w-48 h-28 space-y-2 bg-gradient-to-r from-[#D3A256] to-[#FDE8C0]
                 justify-center items-center rounded-lg flex flex-col">
-                        <p className="text-lg font-bold">20</p>
+                        <p className="text-lg font-bold">{ongoingTasks?.length}</p>
                         <h4 className="text-lg font-semibold">Ongoing Tasks</h4>
                     </div>
                 </div>
@@ -33,7 +40,7 @@ const DashboardHome = () => {
                 <div className="flex justify-center">
                     <div className="w-48 h-28 space-y-2 bg-gradient-to-r from-[#FE4880] to-[#FECDE9]
                 justify-center items-center rounded-lg flex flex-col">
-                        <p className="text-lg font-bold">20</p>
+                        <p className="text-lg font-bold">{completedTasks?.length}</p>
                         <h4 className="text-lg font-semibold">Completed Tasks</h4>
                     </div>
                 </div>
